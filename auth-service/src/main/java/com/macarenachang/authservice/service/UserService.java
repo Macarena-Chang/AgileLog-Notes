c66bd092c4bf38e1dea2b391cf501e2810eac71b
macarena-chang <changmacarena@gmail.com>
2023-08-14 20:12:21 +0200

    Add health check endpoint (Spring Boot Actuator)

--- auth-service/src/main/java/com/macarenachang/authservice/service/UserService.java
+++ auth-service/src/main/java/com/macarenachang/authservice/service/UserService.java
@@ -6,16 +6,20 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import com.macarenachang.authservice.dao.UserRepository;
 import com.macarenachang.authservice.model.User;
-
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     public User register(User user) {
         // Check if email is already registered
@@ -29,20 +33,6 @@ public class UserService {
         return userRepository.save(user);
     } 
 
-/*     public List<User> recuperarUsers() {
-		/*Para simular que tarda mucho tiempo en microservicio-users 
-		 *  ServiceImpl agregar un Thread.sleep(10000)
-		 * y ver cómo funcionan los métodos asincronicos (28)
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	
-		return userRepository.devolverUsers();
-		
-	} */
 
     
 }
